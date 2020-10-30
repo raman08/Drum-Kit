476e4e3e79779a27ab183b1b69187454a6f8ba03
Raman Preet Singh <ramanpre0810@gmail.com>
2020-10-30 23:31:11 +0530

    Rock and Roll

---
+++ drumPlayer.js
@@ -0,0 +1,45 @@
+// Function to play sound
+function playSound(e) {
+
+	// Using the querySelector to find the element with associated data.
+	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+
+	// Finding the key element.
+	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+	// If there is no audio element associated with the key process then return.
+	if (!audio)
+		return;
+
+	//Revind the audio to the start.
+	audio.currentTime = 0
+
+	//Playing the audio element.
+	audio.play();
+
+	//Adding the transition effect to the keys
+	key.classList.add('playing')
+
+}
+
+//Function to remove transition
+function removeTransition(e) {
+
+	//If there is no transformation the skip
+	if (e.propertyName !== 'transform')
+		return;
+
+	// Removing the transformation
+	this.classList.remove('playing')
+
+}
+
+//Finding the audio element associated with the key precess.
+window.addEventListener('keydown', playSound);
+
+// Storing all the keys
+const keys = document.querySelectorAll('.key');
+
+// Looping to all the keys and removing the transition if there is any
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+
